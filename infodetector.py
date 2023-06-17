0e533ec4af1e8e8eb1d4acf259366ff0af5d0a1f
bdilbeck <bdilbec4@students.kennesaw.edu>
2023-06-16 20:24:46 -0400

    Update infodetector.py

--- infodetector.py
+++ infodetector.py
@@ -20,17 +20,19 @@ while True:
     ret, frame = camera.read()
     cv2.imshow("frame", frame)
 
-
-    _, start_frame=camera.read()      
-    inspect = DeepFace.analyze(start_frame, actions=("gender","age","race","emotion"))
-    info["Age"].append(inspect[0]["age"])
-    info["Emotion"].append(inspect[0]["dominant_emotion"])
-    info["Gender"].append(inspect[0]["dominant_gender"])
-    info["Race"].append(inspect[0]["dominant_race"])
-    print(info)
-    time.sleep(30.0) # The script re-downloads the actions of gender, age, race, and emotion every 30 seconds. 
-    if cv2.waitKey(1) == ord('q'):
-        break
+    try:
+        _, start_frame=camera.read()      
+        inspect = DeepFace.analyze(start_frame, actions=("gender","age","race","emotion"))
+        info["Age"].append(inspect[0]["age"])
+        info["Emotion"].append(inspect[0]["dominant_emotion"])
+        info["Gender"].append(inspect[0]["dominant_gender"])
+        info["Race"].append(inspect[0]["dominant_race"])
+        print(info)
+        time.sleep(30.0) # The script re-downloads the actions of gender, age, race, and emotion every 30 seconds. 
+    except ValueError:
+        pass
+        if cv2.waitKey(1) == ord('q'):
+            break
   
 camera.release()
 cv2.destroyAllWindows()
